005f95ce2d8ecf613d23dc12f455b3d2071ea28b
vanvans <Vanessa@wifi-172-23-27-84.uoa-wifi.auckland.ac.nz>
2016-10-17 19:28:42 +1300

    changeColor script added

---
+++ Assets/Resources/changeColour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeColour : MonoBehaviour {
+	public Color startColor;
+	public Color mouseOverColor;
+	bool mouseOver = false;
+
+	// Use this for initialization
+	void OnMouseEnter() {
+		mouseOver = true;
+		GetComponent<Renderer>().material.SetColor ("_Color", mouseOverColor);
+	}
+	
+	// Update is called once per frame
+	void OnMouseExit() {
+		mouseOver = false;
+		GetComponent<Renderer>().material.SetColor ("_Color", startColor);
+	
+	}
+}
